bb61d267beb6187063b75cc16d47c4a0c21247d4
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-16 13:27:50 +0200

    Add project files.

---
+++ Thema3_Klassen_en_Objecten_Opdracht1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/*
+ * Schrijf de code voor de klasse Balk
+ * 
+ * De klasse heeft 4 attributen : typenaam, hoogte, breedte en dikte.
+ * 
+ * De klasse heeft twee OVERLOADED constructors:
+ * 
+ * - Constructor 1 : Geeft de kenmerken een standaard waarde
+ * - Constructor 2 : Geeft de kenmerken de waarde die in de argumentenlijst zijn meegegeven
+ * 
+ * Je voorziet GEEN andere zaken in de code.
+ * 
+ * Vervolgens maak je in de main-routine drie Balk-objecten aan.
+ * - Eén Balk-object 'balk1' DECLAREER je (dus geen 'new' gebruiken)
+ * - Twee Balk-objecten INSTANTIEER je:
+ * 
+ *      'balk2' met de default constructor.
+ *      'balk3' met de overloaded constructor
+ *      
+ * Print de typenaam van elk aangemaakt object in de console. Wat stel je vast?
+ * 
+ * Is een object een REFERENCE-type of VALUE-type? leg uit.
+ * 
+ * Maak balk1=balk3 en print terug de typenaam van elk aangemaakt object in de console. Wat stel je vast?   
+ * * 
+ * */
+
+namespace Thema3_Klassen_en_Objecten_Opdracht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Schrijf hier de code om een object 
+            //te instantiëren
+        }
+    }
+}
